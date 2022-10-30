b7b73197ca750f2a0759cc688421d1c70b215013
leskevich <leskevichtema@gmail.com>
2022-10-30 14:12:52 +0100

    commit user

--- src/User/User.tsx
+++ src/User/User.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 const User = () => {
     return (
         <div>
-            User
+         <h3>User1</h3>
+         <h3>User2</h3>
+         <h3>User3</h3>
+         <h3>User4</h3>
+         <h3>User5</h3>
         </div>
     );
 };
